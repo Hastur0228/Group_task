143012307fa9412dc03d7300e87e4f7aaedf35dd
Hastur0228 <129817989+Hastur0228@users.noreply.github.com>
2023-06-27 23:07:46 +0800

    Update Mission.h

--- Mission.h
+++ Mission.h
@@ -23,7 +23,7 @@ void createuser();//创建用户
 void Complete_help();//展示完整的命令行帮助说明
 void login();//登录函数输入用户名、口令，包含加密过程
 void* check(void *arg);//子线程进行对提醒时间的检查,return NULL即可，不需要从子线程再获取数据
-void FileInput();//读入本地文件，存储到task_arraay中
+void FileInput(string fileplace);//读入本地文件，存储到task_arraay中
 void run();//run了之后，开始执行各种命令的函数，各种命令都包含在这个大函数里面
 void addTask();
 //这个要注意每个任务的开始时间不同，任务名称+开始时间具有唯一性，任务优先级和类型有缺省值
