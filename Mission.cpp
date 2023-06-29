3c3df0092968b0f59c731d8941fa0cd79c12ce86
Hastur0228 <129817989+Hastur0228@users.noreply.github.com>
2023-06-29 17:06:37 +0800

    Update Mission.cpp

--- Mission.cpp
+++ Mission.cpp
@@ -6,19 +6,45 @@
 #include<time.h>
 #include<queue>
 using namespace std;
+
 /*
 待实现函数：
 1.mission类里的所有函数
-2.void Complete_help();//展示"完整"的命令行帮助说明以及使用范例
-3.login()函数
-4.void* check()//子线程函数
-4*.子线程中的检测函数和打印提醒
-5.run（）函数
-6.FileInput函数
+5.run（）函数（改）
+logout函数
+login修改有返回值
 7.addTask()  showTask()  delTask()  clearTask()
-8.synchronize()将本地任务和内存中的任务同步
 */
 
+
+void synchronize(string fileplace,priority_queue<mission> &task_array)
+{
+        FILE *fp = fopen(fileplace,"w");
+
+        if(fp == NULL){
+                cout<<"Error opening file\n";
+                return -1;}
+
+        mission a;
+        while(!task_array.empty()){
+             a = task_array.top();
+             task_array.pop();
+             fputs(a.task_name,fp);
+             fputs(" ",fp);
+             string time;
+             time = to_string( a.do_time.year) + ":" + to_string(a.do_time.month) + ":" +to_string(a.do_time.day)+":"+to_String(a.do_time.hour)+":"+to_string(a.do_time.min)+":"+to_string(a.do_time.sec);
+             fputs(time,fp);
+             fputs(" ",fp);
+              time = to_string( a.remind_time.year) + ":" + to_string(a.remind_time.month) + ":" +to_string(a.remind_time.day)+":"+to_String(a.remind_time.hour)+":"+to_string(a.remind_time.min)+":"+to_string(a.remind_time.sec);
+             fputs(time,fp);
+             fputs(" ",fp);
+             fprintf(fp,"%c ",a.priority);
+             fputs(a.category,fp);
+             fputs(" ",fp);
+             fprintf(fp,"%d\n",a.ID);
+        }
+        return 0;
+}
 //注意每一个操作后都要同步到本地文件，输入的任务，要保存到本地文件；每一个任务输入完成后自动保存到文件；
 void Complete_help() {
 	cout<<"完整的命令行帮助说明和使用范例\n";
@@ -29,17 +55,10 @@ void Complete_help() {
 	cout<<"4.命令行参数：showTask\n";
 	cout<<"5.命令行参数：delTask\n";
 	cout<<"6.命令行参数：clearTask\n";
-	cout<<"7.命令行参数：logout\n";
+	cout<<"7.命令行参数：createuser\n";
+	cout<<"8.命令行参数：login\n";
+	cout<<"9.命令行参数：logout\n";
 	cout<<"8.命令行参数：exit\n";
-	cout<<"使用范例：\n";
-	cout<<"1.命令行参数：run\n";
-	cout<<"2.命令行参数：run -h\n";
-	cout<<"3.命令行参数：run addTask\n";
-	cout<<"4.命令行参数：run showTask\n";
-	cout<<"5.命令行参数：run delTask\n";
-	cout<<"6.命令行参数：run clearTask\n";
-	cout<<"7.命令行参数：run logout\n";
-	cout<<"8.命令行参数：run exit\n";
 }
 
 //子线程后台提醒
@@ -73,7 +92,6 @@ void *check(void *arg){
 	}
 	return NULL;
 }
-
 
 void createuser()
 {
