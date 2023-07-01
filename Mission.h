554b17df03a18ece7eb212a097d4bf4bec6846e0
Hastur0228 <129817989+Hastur0228@users.noreply.github.com>
2023-07-01 11:37:19 +0800

    Update Mission.h

--- Mission.h
+++ Mission.h
@@ -11,7 +11,7 @@ using namespace std;
 /*可能会继续增加
 logout退出登录功能
 */
-
+int g_ID = 0;
 struct Time {
     int year;
     int month;
@@ -28,11 +28,11 @@ struct Time {
         min = mi;
         sec = s;
     }
-    ~Time();
+    ~Time() {};
 };
 
 class mission {
-private:
+public:
     std::string task_name;//任务名称
     Time do_time;//任务开始时间
     Time remind_time;//提醒时间
@@ -41,6 +41,14 @@ private:
     int ID;//分配的ID
 
 public:
+    mission()
+    {
+        task_name = "0";
+        do_time = 0;
+        remind_time = 0;
+        priority = 0;
+        category = "0";
+    }
     mission(std::string t_name, Time d_time, Time r_time, char pri = 'm', std::string cat = "unclassified") {
         //任务名和提醒时间必须输入，优先级和分类不输入就是空，启动时间获取系统时间自动生成，ID自动生成
         task_name = t_name;
@@ -50,15 +58,6 @@ public:
         category = cat;
         //ID赋值在addtask函数中实现，将新生成的misson的ID赋值为最后一个mission的ID+1
     };
-    void show()//三行依次打印 名字 优先级 类别 ； 建立时间 ； 提醒时间
-    {
-        cout<<task_name<<" "<<priority<<" "<<category<<endl;
-        printf("%02d:%02d:%02d %d/%d/%d\n", do_time.hour, do_time.min, do_time.sec, do_time.year, do_time.month, do_time.day);
-        printf("%02d:%02d:%02d %d/%d/%d\n", remind_time.hour, remind_time.min, remind_time.sec, remind_time.year, remind_time.month, remind_time.day);
-    }
-    Time get_reminding_time() {
-        return remind_time;
-    }
     ~mission() { }
     friend int time_cpr(const Time& a, const Time& b);
     friend bool operator<(const mission& a, const mission& b);
@@ -87,23 +86,27 @@ bool operator<(const mission& a, const mission& b) {
         else if (a.priority == 'm') {
             if (b.priority == 'm' || b.priority == 'l')return false;
         }
-        else return true ;
+        else return true;
     }
     else return false;
 }
 
-
+void show(const mission a)//三行依次打印 名字 优先级 类别 ； 建立时间 ； 提醒时间
+{
+    cout << a.task_name << " " << a.priority << " " << a.category << endl;
+    printf("%02d:%02d:%02d %d/%d/%d\n", a.do_time.hour, a.do_time.min, a.do_time.sec, a.do_time.year, a.do_time.month, a.do_time.day);
+    printf("%02d:%02d:%02d %d/%d/%d\n", a.remind_time.hour, a.remind_time.min, a.remind_time.sec, a.remind_time.year, a.remind_time.month, a.remind_time.day);
+}
 void createuser();//创建用户
 void Complete_help();//展示完整的命令行帮助说明
-void login();//登录函数输入用户名、口令，包含加密过程
+string login();//登录函数输入用户名、口令，包含加密过程
 void* check(void* arg);//子线程进行对提醒时间的检查,return NULL即可，不需要从子线程再获取数据
-void FileInput(string fileplace,priority_queue &array_task);//读入本地文件，存储到task_arraay中
-void run();//run了之后，开始执行各种命令的函数，各种命令都包含在这个大函数里面
-void addTask(string fileplace,priority_queue %task_array);
+void FileInput(string fileplace, priority_queue<mission>& array_task);//读入本地文件，存储到task_arraay中
+void run(string fileplace, priority_queue<mission>& array_task);//run了之后，开始执行各种命令的函数，各种命令都包含在这个大函数里面
+void addTask(string fileplace, priority_queue<mission>& task_array);
 //这个要注意每个任务的开始时间不同，任务名称+开始时间具有唯一性，任务优先级和类型有缺省值
 //还有给任务分配唯一的ID
-void showTask(priority_queue task_array);//注意自行设计显示方式
-void delTask(string fileplace,priority_queue &array_task,int delID);//根据任务id，删除某个指定任务
-void clearTask(string fileplace, priority_queue &task_array);//清空任务
-void synchronize(string fileplace,priority_queue<mission> &task_array);//将本地文件和内存task_array中的任务同步*/
-
+void showTask(priority_queue<mission>task_array);//注意自行设计显示方式
+void delTask(string fileplace, priority_queue<mission>& array_task, int delID);//根据任务id，删除某个指定任务
+void clearTask(string fileplace, priority_queue<mission>& task_array);//清空任务
+void synchronize(string fileplace, priority_queue<mission>& task_array);//将本地文件和内存task_array中的任务同步*/
