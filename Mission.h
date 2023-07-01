703450c488e1b9827c2c2178f02bb8dff4df1f30
Yingming Zheng <137150220+8zym@users.noreply.github.com>
2023-07-01 11:32:28 +0800

    Update Mission.h

--- Mission.h
+++ Mission.h
@@ -97,13 +97,13 @@ void createuser();//创建用户
 void Complete_help();//展示完整的命令行帮助说明
 void login();//登录函数输入用户名、口令，包含加密过程
 void* check(void* arg);//子线程进行对提醒时间的检查,return NULL即可，不需要从子线程再获取数据
-void FileInput(string fileplace);//读入本地文件，存储到task_arraay中
+void FileInput(string fileplace,priority_queue &array_task);//读入本地文件，存储到task_arraay中
 void run();//run了之后，开始执行各种命令的函数，各种命令都包含在这个大函数里面
-void addTask();
+void addTask(string fileplace,priority_queue %task_array);
 //这个要注意每个任务的开始时间不同，任务名称+开始时间具有唯一性，任务优先级和类型有缺省值
 //还有给任务分配唯一的ID
-void showTask();//注意自行设计显示方式
-void delTask();//根据任务id，删除某个指定任务
-void clearTask();//清空任务
-void synchronize();//将本地文件和内存task_array中的任务同步*/
+void showTask(priority_queue task_array);//注意自行设计显示方式
+void delTask(string fileplace,priority_queue &array_task,int delID);//根据任务id，删除某个指定任务
+void clearTask(string fileplace, priority_queue &task_array);//清空任务
+void synchronize(string fileplace,priority_queue<mission> &task_array);//将本地文件和内存task_array中的任务同步*/
 
